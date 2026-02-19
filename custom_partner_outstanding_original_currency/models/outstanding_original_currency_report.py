490670094f0b3874e90bd7db2e51701ae8b440af
rovinlr <rovinlr@gmail.com>
2026-02-18 23:04:15 -0600

    Fix outstanding report headers and factura column order

--- custom_partner_outstanding_original_currency/models/outstanding_original_currency_report.py
+++ custom_partner_outstanding_original_currency/models/outstanding_original_currency_report.py
@@ -84,15 +84,18 @@ class OutstandingOriginalCurrencyReportHandler(models.AbstractModel):
                         {
                             "id": report._get_generic_line_id("account.move", move["id"], parent_line_id=currency_line_id),
                             "parent_id": currency_line_id,
-                            "name": self._fmt_date(move["invoice_date"]),
+                            "name": "",
                             "level": 3,
                             "caret_options": "account.move",
                             "class": "o_statement_original_currency_detail",
                             "columns": [
+                                {"name": move["display_number"]},
+                                {
+                                    "name": self._fmt_date(move["invoice_date"])
+                                },
                                 {
                                     "name": self._fmt_date(move["invoice_date_due"])
                                 },
-                                {"name": move["display_number"]},
                                 {
                                     **self._monetary_col(report, move["original_amount"], currency),
                                 },
@@ -111,6 +114,7 @@ class OutstandingOriginalCurrencyReportHandler(models.AbstractModel):
                         "level": 3,
                         "class": "o_statement_original_currency_subtotal",
                         "columns": [
+                            {"name": ""},
                             {"name": ""},
                             {"name": ""},
                             {
