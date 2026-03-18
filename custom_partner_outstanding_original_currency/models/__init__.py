d45fad0d50829d45988af36d563f5e04abbacf10
rovinlr <rovinlr@gmail.com>
2026-03-18 11:08:39 -0600

    Delete custom_partner_outstanding_original_currency directory

--- custom_partner_outstanding_original_currency/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import account_move
-from . import account_report
-from . import outstanding_original_currency_report
-from . import res_partner
--- custom_partner_outstanding_original_currency/models/account_move.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class AccountMove(models.Model):
-    _inherit = "account.move"
-
-    fp_consecutive_number = fields.Char(string="Consecutive Number")
